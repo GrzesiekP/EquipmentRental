0b810679d154037a447ee086f88177f7a8f1bcde
Grzegorz Pawlowski <grzegorz.pawlowski@avantida.com>
2022-01-28 18:29:41 +0100

    Add todo

--- Orders/ValueObjects/OrderData.cs
+++ Orders/ValueObjects/OrderData.cs
@@ -5,5 +5,11 @@ namespace Orders.ValueObjects
     public class OrderData
     {
         public List<EquipmentItem> Equipment { get; set; }
+        
+        // TODO:
+        // public DateTime RentalDate { get; set; }
+        // public DateTime ReturnDate { get; set; }
+        // public Guid RentingUserId { get; set; }
+        // public decimal CalculateTotalPrice()
     }
 }
